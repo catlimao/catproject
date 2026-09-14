fe77a8e8532fabcf2b0b0eb7e84e2d994270ff4a
catlimao <1195387011@qq.com>
2026-09-14 15:39:16 +0800

    Replace blurry role artwork

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import { assetBase, catSuffixes, questions, roles, traits, uiCopy } from './data
 import { buildResultFromSlug, calculateResult, createShareText, getAllHiddenResults, getAllNormalResults } from './lib/scoring';
 import { clearDraft, readDraft, readLastResult, readUnlockedResults, saveDraft, saveLastResult, unlockResult } from './lib/storage';
 
-const assetVersion = '20260914a';
+const assetVersion = '20260914b';
 const versionedAsset = (path) => `${assetBase}${path}?v=${assetVersion}`;
 
 function getHashRoute() {
